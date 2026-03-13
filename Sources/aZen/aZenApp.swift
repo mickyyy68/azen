e6ffb19ba65b2498c85c12a5c39b82c8ea8d940c
mickyyy68 <aristarcomichael26@gmail.com>
2026-03-13 10:25:28 +0100

    Replace scope icon with "aZ" logo for menu bar and app icon

--- Sources/aZen/aZenApp.swift
+++ Sources/aZen/aZenApp.swift
@@ -19,6 +19,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     private var cancellables = Set<AnyCancellable>()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.applicationIconImage = makeAppIcon()
         setupStatusItem()
         setupPillWindow()
         setupHotkey()
@@ -30,7 +31,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     private func setupStatusItem() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let button = statusItem.button {
-            button.image = NSImage(systemSymbolName: "scope", accessibilityDescription: "aZen")
+            button.image = makeMenuBarIcon()
         }
 
         let menu = NSMenu()
@@ -100,12 +101,55 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     private func updateStatusIcon(state: SessionState) {
-        var image = NSImage(systemSymbolName: "scope", accessibilityDescription: "aZen")
-        if state != .idle && state != .setup {
-            let config = NSImage.SymbolConfiguration(pointSize: 14, weight: .medium)
-                .applying(.init(paletteColors: [.controlAccentColor]))
-            image = image?.withSymbolConfiguration(config)
+        if let button = statusItem.button {
+            button.image = makeMenuBarIcon()
+            button.contentTintColor = (state != .idle && state != .setup)
+                ? .controlAccentColor
+                : nil
         }
-        statusItem.button?.image = image
+    }
+
+    // MARK: - Icon Rendering
+
+    private func makeMenuBarIcon() -> NSImage {
+        let text = "aZ" as NSString
+        let font = NSFont.systemFont(ofSize: 14, weight: .bold)
+        let attrs: [NSAttributedString.Key: Any] = [
+            .font: font,
+            .foregroundColor: NSColor.black
+        ]
+        let size = text.size(withAttributes: attrs)
+        let image = NSImage(size: size, flipped: false) { rect in
+            text.draw(in: rect, withAttributes: attrs)
+            return true
+        }
+        image.isTemplate = true
+        return image
+    }
+
+    private func makeAppIcon() -> NSImage {
+        let size: CGFloat = 512
+        let image = NSImage(size: NSSize(width: size, height: size), flipped: false) { rect in
+            let bgPath = NSBezierPath(roundedRect: rect, xRadius: size * 0.22, yRadius: size * 0.22)
+            NSColor.black.setFill()
+            bgPath.fill()
+
+            let text = "aZ" as NSString
+            let font = NSFont.systemFont(ofSize: size * 0.42, weight: .bold)
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: font,
+                .foregroundColor: NSColor.white
+            ]
+            let textSize = text.size(withAttributes: attrs)
+            let textRect = NSRect(
+                x: (size - textSize.width) / 2,
+                y: (size - textSize.height) / 2,
+                width: textSize.width,
+                height: textSize.height
+            )
+            text.draw(in: textRect, withAttributes: attrs)
+            return true
+        }
+        return image
     }
 }
